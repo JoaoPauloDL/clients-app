dcbc00943c0ea3517f3636e24369fbc5b18f03ab
João Paulo <joaopaulomayka@gmail.com>
2022-09-19 14:22:14 -0300

    adição de feedback ao emitir um serviço

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -12,6 +12,8 @@ import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
 export class ServicoPrestadoFormComponent implements OnInit {
   clientes: Cliente[] = [];
   servico: ServicoPrestado;
+  success: boolean = false;
+  errors: String[];
 
   constructor(
     private clientesService: ClientesService,
@@ -29,6 +31,16 @@ export class ServicoPrestadoFormComponent implements OnInit {
   onSubmit() {
     this.service
       .salvar(this.servico)
-      .subscribe((response) => console.log(response));
+      .subscribe(
+        (response) => {
+          this.success = true;
+          this.errors = null;
+          this.servico = new ServicoPrestado();
+        },
+        (errorResponse) => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        }
+      );
   }
 }
