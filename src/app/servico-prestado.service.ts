ec8b014dd1a539a827ba88cae60541cf4376f568
João Paulo <joaopaulomayka@gmail.com>
2022-09-20 18:00:54 -0300

    criação do get de servicos prestados

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -1,23 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { environment } from 'src/environments/environment';
-
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServicoPrestadoService {
-
   private baseApiUrl = environment.baseApiUrl;
   private apiUrl = `${this.baseApiUrl}api/servicos-prestados`;
 
-  constructor(
-    private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado>{
-    return this.http.post<ServicoPrestado>(this.apiUrl, servicoPrestado)
+  salvar(servicoPrestado: ServicoPrestado): Observable<ServicoPrestado> {
+    return this.http.post<ServicoPrestado>(this.apiUrl, servicoPrestado);
   }
 
+  buscar(nome: string, mes: number): 
+  Observable<ServicoPrestadoBusca[]> {
+    const httpParams =  new HttpParams()
+    .set("nome", nome)
+    .set("mes", mes.toString());
+  
+    const url = this.apiUrl + "?" + httpParams.toString();
+    console.log(url)
+    return this.http.get<any>(url)
+  }
 }
